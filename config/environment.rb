a2125d1fd43fdca0d70a8d07c842e8f988a4cc4f
HuniaNad <nadeemsonaina987@gmail.com>
2023-11-12 18:35:25 +0500

    Updated production settings

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ENV['QUORACLONE_DATABASE_USERNAME'] = 'postgres'
-ENV['QUORACLONE_DATABASE_PASSWORD'] = '12345'
